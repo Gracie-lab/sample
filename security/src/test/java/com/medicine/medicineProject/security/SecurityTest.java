f7caec3ee19b001297b6ff2dc83f899c5624565d
Grace <gracebolutife@gmail.com>
2021-03-26 08:50:21 +0100

    .

--- security/src/test/java/com/medicine/medicineProject/security/SecurityTest.java
+++ security/src/test/java/com/medicine/medicineProject/security/SecurityTest.java
@@ -1,34 +1,34 @@
-package com.medicine.medicineProject.security;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-//@SpringBootTest(classes = SecurityConfig.class)
-@RunWith(SpringRunner.class)
-@WebAppConfiguration
-class SecurityTest {
-
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    WebApplicationContext webApplicationContext;
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .build();
-
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-}
+//package com.medicine.medicineProject.security;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.web.context.WebApplicationContext;
+//
+////@SpringBootTest(classes = SecurityConfig.class)
+//@RunWith(SpringRunner.class)
+//@WebAppConfiguration
+//class SecurityTest {
+//
+//    @Autowired
+//    MockMvc mockMvc;
+//
+//    @Autowired
+//    WebApplicationContext webApplicationContext;
+//
+//    @BeforeEach
+//    void setUp() {
+//        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//                .build();
+//
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+//}
--- security/src/test/java/com/medicine/medicineProject/security/UserPrincipalServiceTest.java
+++ security/src/test/java/com/medicine/medicineProject/security/UserPrincipalServiceTest.java
@@ -1,52 +1,52 @@
-package com.medicine.medicineProject.security;
-
-import com.medicine.medicineProject.config.SecurityConfig;
-import com.medicine.medicineProject.models.Patient;
-import com.medicine.medicineProject.repositories.PatientRepository;
-import com.medicine.medicineProject.repositories.UserRepository;
-//import com.medicine.medicineProject.service.UserPrincipalService;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(classes = SecurityConfig.class)
-@Slf4j
-class UserPrincipalServiceTest {
-
-    @Autowired
-    PatientRepository patientRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
+//package com.medicine.medicineProject.security;
+//
+//import com.medicine.medicineProject.config.SecurityConfig;
+//import com.medicine.medicineProject.models.Patient;
+//import com.medicine.medicineProject.repositories.PatientRepository;
+//import com.medicine.medicineProject.repositories.UserRepository;
+////import com.medicine.medicineProject.service.UserPrincipalService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@SpringBootTest(classes = SecurityConfig.class)
+//@Slf4j
+//class UserPrincipalServiceTest {
+//
 //    @Autowired
-//    UserPrincipalService userPrincipalService;
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void can_load_user_by_username(){
-        Patient patient = new Patient();
-        patient.setEmail("grace@gmail.com");
-        patient.setPassword("password");
-        patientRepository.save(patient);
-        userRepository.save(patient);
-
-//      UserDetails userDetails = userPrincipalService.loadUserByUsername("grace@gmail.com");
-//        log.info("User details -> {}", userDetails);
-//        assertThat(userDetails.getUsername()).isEqualTo("grace@gmail.com");
-
-    }
-}
+//    PatientRepository patientRepository;
+//
+//    @Autowired
+//    UserRepository userRepository;
+//
+////    @Autowired
+////    UserPrincipalService userPrincipalService;
+//
+//    @BeforeEach
+//    void setUp() {
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+//
+//    @Test
+//    void can_load_user_by_username(){
+//        Patient patient = new Patient();
+//        patient.setEmail("grace@gmail.com");
+//        patient.setPassword("password");
+//        patientRepository.save(patient);
+//        userRepository.save(patient);
+//
+////      UserDetails userDetails = userPrincipalService.loadUserByUsername("grace@gmail.com");
+////        log.info("User details -> {}", userDetails);
+////        assertThat(userDetails.getUsername()).isEqualTo("grace@gmail.com");
+//
+//    }
+//}
