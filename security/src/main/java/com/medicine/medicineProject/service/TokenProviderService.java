8ea8de792d2518a9e9e0db7554104a89bf7b3325
Grace <gracebolutife@gmail.com>
2021-03-26 08:47:48 +0100

    .

--- security/src/main/java/com/medicine/medicineProject/service/TokenProviderService.java
+++ security/src/main/java/com/medicine/medicineProject/service/TokenProviderService.java
@@ -1,24 +1,24 @@
-package com.medicine.medicineProject.service;
-
-import com.medicine.medicineProject.models.User;
-import io.jsonwebtoken.Claims;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.function.Function;
-
-public interface TokenProviderService {
-    String getEmailFromToken(String token);
+//package com.medicine.medicineProject.service;
 //
-//    Date getExpiryDateFromToken(String token);
+//import com.medicine.medicineProject.models.User;
+//import io.jsonwebtoken.Claims;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.userdetails.UserDetails;
 //
+//import java.util.function.Function;
+//
+//public interface TokenProviderService {
+//    String getEmailFromToken(String token);
+////
+////    Date getExpiryDateFromToken(String token);
+////
+////    String generateLoginToken(Authentication authentication, User user);
+//
+//    <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver);
+//
+//    public UsernamePasswordAuthenticationToken getAuthentication(final String authenticationToken, final Authentication authentication, final UserDetails userDetails);
+//
+//    boolean validateToken(String token, UserDetails userDetails);
 //    String generateLoginToken(Authentication authentication, User user);
-
-    <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver);
-
-    public UsernamePasswordAuthenticationToken getAuthentication(final String authenticationToken, final Authentication authentication, final UserDetails userDetails);
-
-    boolean validateToken(String token, UserDetails userDetails);
-    String generateLoginToken(Authentication authentication, User user);
-    }
+//    }
